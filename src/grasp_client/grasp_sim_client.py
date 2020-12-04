4630e6af32bdb8f20ee9e3648574aa0900bfa848
Vincent Mayer <vincent.mayer@agile-robots.com>
2020-12-04 16:10:22 +0100

    break: Added plan_hithand_preshape_client in grasp_client.py but not implemented

--- src/grasp_client/grasp_sim_client.py
+++ src/grasp_client/grasp_sim_client.py
@@ -84,12 +84,12 @@ class GraspClient():
 
     def update_gazebo_object_client(self, object_name, object_pose,
                                     object_model_name, model_type, dataset):
-        '''
-            Gazebo management client, deletes previous object and spawns new object
-        '''
+        """Gazebo management client, deletes previous object and spawns new object
+        """
         rospy.loginfo('Waiting for service update_gazebo_object.')
         rospy.wait_for_service('update_gazebo_object')
         rospy.loginfo('Calling service update_gazebo_object.')
+        object_pose_array = self.get_pose_array_from_stamped(object_pose)
         try:
             update_gazebo_object = rospy.ServiceProxy('update_gazebo_object',
                                                       UpdateObjectGazebo)
