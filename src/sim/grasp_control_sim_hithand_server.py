1598be55c9ee02588b5ececb4e4629577129650c
Vincent Mayer <vincent.mayer@agile-robots.com>
2020-12-23 08:01:04 +0100

    fix: hithand grasp controller

--- src/sim/grasp_control_sim_hithand_server.py
+++ src/sim/grasp_control_sim_hithand_server.py
@@ -3,8 +3,11 @@ import rospy
 from grasp_pipeline.srv import *
 from sensor_msgs.msg import JointState
 import numpy as np
+from copy import deepcopy
 # The goal here is to emulate a velocity controller via a pos interface to the hithand. The goal for the hithand is to close at a constant velocity, but it should stop when the velocity is zero.
 # Nevertheless it should maintain a certain force on the object.
+
+DEBUG = False
 
 
 class HithandGraspController():
@@ -16,13 +19,13 @@ class HithandGraspController():
         self.joint_command_pub = rospy.Publisher('/hithand/joint_cmd', JointState, queue_size=1)
 
         self.current_pos_joints = None
-        self.delta_pos_joint_1 = 0.02  # 2rad/s, when sending at 100 Hz
-        self.delta_pos_joint_2 = 0.01  # 1rad/s, when sending at 100 Hz
-        self.delta_pos_joint_3 = 0.005  # 0.5rad/s, when sending at 100 Hz
+        self.delta_pos_joint_1 = 2. / self.freq  # 2rad/s, when sending at 100 Hz
+        self.delta_pos_joint_2 = 1. / self.freq  # 1rad/s, when sending at 100 Hz
+        self.delta_pos_joint_3 = 1. / self.freq  # 0.5rad/s, when sending at 100 Hz
         self.delta_pos_joint_vector = None
-        self.pos_thresh = 0.01  # If a joint moved less than this, it is considered to have zero velocity
+        self.pos_thresh = 0.02  # If a joint moved less than this, it is considered to have zero velocity
 
-        self.check_delta_joint_interval = 5
+        self.check_delta_joint_interval = 10
 
     def init_delta_joint_vector(self):
         self.delta_vector = np.array([
@@ -34,27 +37,31 @@ class HithandGraspController():
         joint_state = rospy.wait_for_message('/hithand/joint_states', JointState, 1)
         if joint_state == None:
             rospy.logerr('No hithand joint state has been received')
-        self.current_pos_joints = np.array(joint_state.pos)
+        self.current_pos_joints = np.array(joint_state.position)
 
     def handle_grasp_control_hithand(self, req):
         self.init_delta_joint_vector()
+        #print(self.delta_pos_joint_vector)
+
         self.update_current_pos_joints()
         joint_pub_msg = JointState()
-        previous_pos_joints = self.current_pos_joints
-        desired_pos_joints = previous_pos_joints
+        previous_pos_joints = deepcopy(self.current_pos_joints)
+        desired_pos_joints = deepcopy(previous_pos_joints)
         for i in xrange(10 * self.freq):  # this loop runs for 10 seconds max
-            if i % self.check_delta_joint_interval == 0:
+            #print(i)
+            if (i % self.check_delta_joint_interval == 0) and (i != 0):
                 self.update_current_pos_joints()
                 # check whether the joint position a few steps ago actually differs from the current joint position. If not velocity is zero and joint should stop moving
                 diff_joint_pos = np.abs(self.current_pos_joints - previous_pos_joints)
                 # set the deltas to zero for the joints with zero velocity in order to not send more position commands to these joints
+                #print(self.delta_pos_joint_vector)
                 self.delta_pos_joint_vector[diff_joint_pos < self.pos_thresh] = 0
+                #print(self.delta_pos_joint_vector)
                 # If the whole delta pos joint vector is zero break the loop, because no joints are moving anymore
                 if sum(self.delta_pos_joint_vector) == 0:
                     break
                 # Update the previous pos joint variable to now be the current joint position
                 previous_pos_joints = self.current_pos_joints
-
             desired_pos_joints += self.delta_pos_joint_vector
             joint_pub_msg.position = desired_pos_joints.tolist()
             self.joint_command_pub.publish(joint_pub_msg)
@@ -68,5 +75,7 @@ class HithandGraspController():
 
 if __name__ == "__main__":
     hgc = HithandGraspController()
+    if DEBUG:
+        hgc.handle_grasp_control_hithand(GraspControlRequest())
     hgc.create_grasp_control_hithand_server()
     rospy.spin()
