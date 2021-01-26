8470e323a967a5bdc739b3be01b96fc81916c974
v.mayer <vincent.mayer@agile-robots.com>
2021-01-26 12:36:42 +0100

    feat: update moveit scene (delete + spawn mesh)

--- src/manage_moveit_scene_server.py
+++ src/manage_moveit_scene_server.py
@@ -34,21 +34,36 @@ class ManageSceneInMoveit():
         response.success = True
         return response
 
-    def create_moveit_scene_server(self):
-        rospy.Service('create_moveit_scene', ManageMoveitScene,
-                      self.handle_create_moveit_scene)
+    def handle_update_moveit_scene(self, req):
+        scene = PlanningSceneInterface()
+        rospy.sleep(0.1)
+        scene.remove_world_object('obj_mesh')
+        rospy.sleep(0.1)
+        scene.add_mesh('obj_mesh', req.object_pose_world, req.object_mesh_path)
+        rospy.sleep(0.1)
+        response = ManageMoveitSceneResponse()
+        response.success = True
+        return response
+
+    def create_create_moveit_scene_server(self):
+        rospy.Service('create_moveit_scene', ManageMoveitScene, self.handle_create_moveit_scene)
         rospy.loginfo('Service create_scene:')
         rospy.loginfo('Ready to create the moveit_scene.')
 
-    def clean_moveit_scene_server(self):
-        rospy.Service('clean_moveit_scene', ManageMoveitScene,
-                      self.handle_clean_moveit_scene)
+    def create_clean_moveit_scene_server(self):
+        rospy.Service('clean_moveit_scene', ManageMoveitScene, self.handle_clean_moveit_scene)
         rospy.loginfo('Service clean_scene:')
         rospy.loginfo('Ready to clean the moveit_scene.')
+
+    def create_update_moveit_scene_server(self):
+        rospy.Service('update_moveit_scene', ManageMoveitScene, self.handle_update_moveit_scene)
+        rospy.loginfo('Service update_moveit_scene:')
+        rospy.loginfo('Ready to update the moveit_scene.')
 
 
 if __name__ == '__main__':
     ms = ManageSceneInMoveit()
-    ms.create_moveit_scene_server()
-    ms.clean_moveit_scene_server()
+    #ms.create_create_moveit_scene_server()
+    #ms.create_clean_moveit_scene_server()
+    ms.create_update_moveit_scene_server()
     rospy.spin()
