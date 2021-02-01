75c381a02b8f77f19b4991deb5e7affb59dc5672
v.mayer <vincent.mayer@agile-robots.com>
2021-02-01 08:36:12 +0100

    feat: pipeline for generating grasp data automatically

--- src/grasp_client/data_gen_grasp_sim_client.py
+++ src/grasp_client/data_gen_grasp_sim_client.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
 import rospy
 from grasp_sim_client import GraspClient
+import os
+import shutil
+# object_name, object_model_name, model_type, dataset
+# res = update_gazebo_object(object_name, object_pose_array, object_model_name, model_type, dataset)
+# req.object_mesh_path = self.spawned_object_mesh_path
 
-object_name, object_model_name, model_type, dataset
-res = update_gazebo_object(object_name, object_pose_array, object_model_name, model_type, dataset)
-req.object_mesh_path = self.spawned_object_mesh_path
+# self.color_img_save_path = rospy.get_param('color_img_save_path')
+# self.depth_img_save_path = rospy.get_param('depth_img_save_path')
+# self.object_pcd_path = rospy.get_param('object_pcd_path')
+# self.scene_pcd_path = rospy.get_param('scene_pcd_path')
 
 
 def choose_grasp_object():
-    """ Chooses a specific object from all objects available and returns 
+    """ Chooses a specific object from all objects available and returns a dict with all relevant metadata.
     """
     object_name = None
     object_mesh_path = None
+    dataset = None
 
     object_metadata = dict()
-    object_metadata["object_name"] = object_name
-    object_metadata["object_mesh_path"] = object_mesh_path
+    object_metadata["name"] = object_name
+    object_metadata["mesh_path"] = object_mesh_path
+    object_metadata["dataset"] = dataset
+    object_metadata["pose_world"] = None
 
     rospy.loginfo('Trying to grasp object: %s' % object_name)
 
@@ -24,9 +33,12 @@ def choose_grasp_object():
 
 if __name__ == '__main__':
     # Create the generic GraspClient, wrapper around all sorts of functionalities
-    grasp_client = GraspClient()
+    data_recording_path = '/home/vm/'
+    shutil.rmtree('/home/vm/grasp_data')
+    grasp_client = GraspClient(grasp_data_recording_path=data_recording_path)
     num_grasps_per_pose = 5  # how many sampled grasp poses to evaluate for object in same position
     while True:
+        grasp_client.create_dirs_new_grasp_trial()
         # Reset panda and hithand
         grasp_client.reset_hithand_and_panda()
 
@@ -47,9 +59,12 @@ if __name__ == '__main__':
 
         for i in xrange(num_grasps_per_pose):
             if i != 0:  # Spawn object in same pose, just choose different grasp configuration. No need for segmentation etc as it won't be different
+
                 grasp_client.reset_hithand_and_panda()
 
                 grasp_client.spawn_object(generate_random_pose=False)
+
+                grasp_client.save_only_depth_and_color(grasp_phase='pre')
 
             # Grasp types can be either unspecified, top, or side
             grasp_type = 'unspecified'
@@ -62,5 +77,3 @@ if __name__ == '__main__':
 
             # Save all grasp data including post grasp images
             grasp_client.save_data_post_grasp()
-
-            # Save grasping experiment data
