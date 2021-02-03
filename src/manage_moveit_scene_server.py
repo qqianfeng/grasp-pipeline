1ac98311e0b70e53221db6066cb4f482c4a7de5b
v.mayer <vincent.mayer@agile-robots.com>
2021-02-03 09:25:54 +0100

    feat: default add ground plane to moveit

--- src/manage_moveit_scene_server.py
+++ src/manage_moveit_scene_server.py
@@ -12,6 +12,39 @@ class ManageSceneInMoveit():
     def __init__(self):
         rospy.init_node('manage_moveit_scene_node')
         self.use_sim = rospy.get_param('~sim')
+        self.add_ground_plane()
+
+    def add_ground_plane(self):
+        rospy.loginfo("Adding ground plane to planning scene.")
+        scene = PlanningSceneInterface()
+        rospy.sleep(0.5)
+        pose = PoseStamped()
+        pose.header.frame_id = "world"
+        pose.pose.orientation.w = 1
+        scene.add_plane("ground_plane", pose)
+
+        rospy.sleep(0.5)
+        start = rospy.get_time()
+        seconds = rospy.get_time()
+        timeout = 5
+        while (seconds - start < timeout) and not rospy.is_shutdown():
+            attached_objects = scene.get_attached_objects(["ground_plane"])
+            is_attached = len(attached_objects.keys()) > 0
+
+            is_known = "ground_plane" in scene.get_known_object_names()
+
+            # Test if we are in the expected state
+            if is_attached or is_known:
+                rospy.loginfo("Ground plane is now known to moveit.")
+                return True
+
+            # Sleep so that we give other threads time on the processor
+            rospy.sleep(0.2)
+            seconds = rospy.get_time()
+
+        # If we exited the while loop without returning then we timed out
+        rospy.loginfo("Ground plane is not known to moveit.")
+        return False
 
     def handle_create_moveit_scene(self, req):
         scene = PlanningSceneInterface()
@@ -36,11 +69,11 @@ class ManageSceneInMoveit():
 
     def handle_update_moveit_scene(self, req):
         scene = PlanningSceneInterface()
-        rospy.sleep(0.1)
+        rospy.sleep(0.5)
         scene.remove_world_object('obj_mesh')
-        rospy.sleep(0.1)
+        rospy.sleep(0.5)
         scene.add_mesh('obj_mesh', req.object_pose_world, req.object_mesh_path)
-        rospy.sleep(0.1)
+        rospy.sleep(0.5)
         response = ManageMoveitSceneResponse()
         response.success = True
         return response
