d638f7fd2c76bc6c344bacd84fb776ac5c1bd97c
v.mayer <vincent.mayer@agile-robots.com>
2021-02-16 22:07:22 +0100

    feat: record grasp data for new sampling method

---
+++ src/sim/record_grasp_and_collision_data_server.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python
+import rospy
+from grasp_pipeline.srv import *
+from geometry_msgs.msg import Pose, Quaternion, PoseStamped
+from sensor_msgs.msg import JointState
+import h5py
+import numpy as np
+from datetime import datetime
+import os
+
+
+class RecordGraspData():
+    def __init__(self):
+        if not DEBUG:
+            rospy.init_node('record_grasp_and_collision_data_node')
+            self.data_recording_path = rospy.get_param('data_recording_path', '/home/vm')
+        else:
+            self.data_recording_path = '/home/vm/'
+        self.grasp_data_file_name = os.path.join(self.data_recording_path, 'grasp_data.h5')
+        self.recording_session_id = None
+        self.initialize_data_file()
+        self.collision_id = 0
+        self.non_collision_id = 0
+
+    ###################################
+    ######  PART I : Helper  ##########
+    ###################################
+    def convert_pose_to_list(self, pose):
+        return [
+            pose.pose.position.x, pose.pose.position.y, pose.pose.position.z,
+            pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z,
+            pose.pose.orientation.w
+        ]
+
+    ###################################
+    ###  PART II : Initializations  ###
+    ###################################
+    def initialize_object_name_metadata_group(self, metadata_gp):
+        metadata_gp.create_dataset("object_num_grasps", data=0, dtype='u4')
+        metadata_gp.create_dataset("object_num_tops", data=0, dtype='u4')
+        metadata_gp.create_dataset("object_num_successes", data=0, dtype='u4')
+        metadata_gp.create_dataset("object_num_failures", data=0, dtype='u4')
+        metadata_gp.create_dataset("object_num_collisions", data=0, dtype='u4')
+        self.collision_id = 0
+        self.non_collision_id = 0
+
+    def initialize_sess_metadata(self, sess_metadata_gp):
+        sess_metadata_gp.create_dataset("sess_start", data=datetime.now().isoformat())
+        sess_metadata_gp.create_dataset("sess_num_grasps", data=0, dtype='u4')
+        sess_metadata_gp.create_dataset("sess_num_tops", data=0, dtype='u4')
+        sess_metadata_gp.create_dataset("sess_num_successes", data=0, dtype='u4')
+        sess_metadata_gp.create_dataset("sess_num_failures", data=0, dtype='u4')
+        sess_metadata_gp.create_dataset("sess_num_collisions", data=0, dtype='u4')
+
+    def initialize_file_metadata(self, metadata_gp):
+        metadata_gp.create_dataset("datetime_recording_start",
+                                   data=[datetime.now().isoformat()],
+                                   maxshape=(None, ))
+        metadata_gp.create_dataset("total_num_grasps", data=0, dtype='u4')
+        metadata_gp.create_dataset("total_num_tops", data=0, dtype='u4')
+        metadata_gp.create_dataset("total_num_successes", data=0, dtype='u4')
+        metadata_gp.create_dataset("total_num_failures", data=0, dtype='u4')
+        metadata_gp.create_dataset("total_num_collisions", data=0, dtype='u4')
+        metadata_gp.create_dataset("total_num_recordings", data=1, dtype='u4')
+
+    def initialize_data_file(self):
+        """ Creates a new grasp file under grasp_data_file_name or opens existing one and then creates "grasp_trials" and "grasp_metadata" groups if they do not yet exist.
+        """
+        # a: Read/write if exists, create otherwise
+        with h5py.File(self.grasp_data_file_name, 'a') as file:
+            # Either create the whole file structure if file did not exist before:
+            if 'recording_sessions' not in file.keys():
+                # grasp_data.h5 --> recording_sessions Create recording_sessions group
+                recording_sessions_gp = file.create_group('recording_sessions')
+                # recording_sessions --> recording_session1
+                self.curr_sess_name = 'recording_session_0001'
+                recording_session_1_gp = recording_sessions_gp.create_group(self.curr_sess_name)
+                # recording_sessions1 --> (metadata and grasp_trials)
+                sess_metadata = recording_session_1_gp.create_group('metadata')
+                recording_session_1_gp.create_group('grasp_trials')
+                # Initialize metadata for current session
+                self.initialize_sess_metadata(sess_metadata)
+
+                # grasp_data.h5 --> metadata Create overall metadata
+                metadata_gp = file.create_group('metadata')
+
+                # Initalize all the variables to be stored in metadata
+                self.initialize_file_metadata(metadata_gp)
+
+                # set current sess name
+                self.curr_sess_name = 'recording_session_0001'
+
+            # If recording_sessions group exists, just create a new subgroup
+            elif 'recording_sessions' in file.keys():
+                file['metadata']['total_num_recordings'][()] += 1
+                self.curr_sess_name = 'recording_session_' + str(
+                    file['metadata']['total_num_recordings'][()]).zfill(4)
+                recording_start_ds = file["metadata"]["datetime_recording_start"]
+                recording_start_ds.resize((recording_start_ds.shape[0] + 1, ))
+                recording_start_ds[-1] = datetime.now().isoformat()
+
+                # recording_sessions --> recordings_session_name
+                recording_session_n_gp = file['recording_sessions'].create_group(
+                    self.curr_sess_name)
+                # recording_sessions_name --> (metadata and grasp_trials)
+                sess_metadata = recording_session_n_gp.create_group('metadata')
+                recording_session_n_gp.create_group('grasp_trials')
+                # Initialize metadata for current session
+                self.initialize_sess_metadata(sess_metadata)
+
+    ###################################
+    ######  PART III : Update  ########
+    ###################################
+    def update_all_metadata(self, grasp_file, is_top_grasp, grasp_success_label, collision=False):
+        # Overall meta data and grasp trial metadata
+        metadata_group = grasp_file['metadata']
+        curr_sess_metadata_group = grasp_file['recording_sessions'][
+            self.curr_sess_name]['metadata']
+
+        metadata_group['total_num_grasps'][()] += 1
+        curr_sess_metadata_group['sess_num_grasps'][()] += 1
+        if is_top_grasp:
+            metadata_group['total_num_tops'][()] += 1
+            curr_sess_metadata_group['sess_num_tops'][()] += 1
+        if collision:
+            metadata_group['total_num_collisions'][()] += 1
+            curr_sess_metadata_group['total_num_collisions'][()] += 1
+
+        if grasp_success_label:
+            metadata_group['total_num_successes'][()] += 1
+            curr_sess_metadata_group['sess_num_successes'][()] += 1
+        else:
+            metadata_group['total_num_failures'][()] += 1
+            curr_sess_metadata_group['sess_num_failures'][()] += 1
+
+    def update_all_metadata_collision(self, grasp_file, num_grasps):
+        # Update overall metadata
+        grasp_file['metadata']['total_num_grasps'][()] += num_grasps
+        grasp_file['metadata']['total_num_collisions'][()] += num_grasps
+
+        # Update session metadata
+        rec_sess_metadata_gp = grasp_file['recording_sessions'][self.curr_sess_name]['metadata']
+        rec_sess_metadata_gp['sess_num_grasps'][()] += num_grasps
+        rec_sess_metadata_gp['sess_num_collisions'][()] += num_grasps
+
+        # Update object
+        object_metadata_gp = grasp_file['recording_sessions'][self.curr_sess_name]['grasp_trials'][
+            self.curr_object_name]['metadata']
+        object_metadata_gp['object_num_grasps'][()] += num_grasps
+        object_metadata_gp['object_num_collisions'][()] += num_grasps
+
+    def get_grasp_group(self, grasp_trials, grasp_class):
+        """ Creates the entire folder structure under grasp_trials if it does not exist.
+        Returns the 
+        """
+        object_name = self.curr_object_name
+
+        # If this object name is not under grasp_trials already, create folder structure
+        if object_name not in grasp_trials.keys():
+            # grasp_trials --> object_name
+            object_gp = grasp_trials.create_group(object_name)
+            # object_name --> metadata
+            metadata_gp = object_gp.create_group('metadata')
+            self.initialize_object_name_metadata_group(metadata_gp)
+            # object_name --> grasps
+            grasps_gp = object_gp.create_group('grasps')
+            # grasps --> collision/ no-collision
+            grasps_gp.create_group('collision')
+            grasps_gp.create_group('no_collision')
+
+        # Get the descriptor for the grasp group
+        grasp_group = grasp_trials[object_name]['grasps'][grasp_class]
+
+        # return either collision group or non-collision
+        return grasp_group
+
+    ###################################
+    ######  PART IV : Services ########
+    ###################################
+    def handle_record_grasp_trial_data(self, req):
+        # r+ : Read/write, file must exist
+        with h5py.File(self.grasp_data_file_name, 'r+') as grasp_file:
+            # workflow: Create new group under grasp trials. Create dataset for each piece of data stored for this trial
+            # 1. Update grasp meta information and update sess metadata
+            self.update_all_metadata(grasp_file, req.is_top_grasp, req.grasp_success_label)
+            grasp_trial_id_str = str(grasp_file['metadata']['total_num_grasps'][()]).zfill(5)
+
+            # 2. Create new group under curr sess grasp_trials with the name e.g. grasp_000005 if 5th grasp
+            grasp_group = grasp_file['recording_sessions'][
+                self.curr_sess_name]['grasp_trials'].create_group('grasp_' + grasp_trial_id_str)
+
+            # 3. Add all the grasp-specific data and store in datasets under new group
+            # object_name
+            grasp_group.create_dataset('object_name', data=req.object_name)
+            # time stamp
+            grasp_group.create_dataset('time_stamp', data=req.time_stamp)
+            # is_top_grasp
+            grasp_group.create_dataset('is_top_grasp', data=req.is_top_grasp)
+            # grasp_success_label
+            grasp_group.create_dataset('grasp_success_label', data=req.grasp_success_label)
+
+            # 4. Record...
+            # object_world_sim_pose
+            grasp_group.create_dataset('object_world_sim_pose',
+                                       data=self.convert_pose_to_list(req.object_world_sim_pose))
+            # desired_preshape_palm_world_pose
+            grasp_group.create_dataset('desired_preshape_palm_world_pose',
+                                       data=self.convert_pose_to_list(
+                                           req.desired_preshape_palm_world_pose))
+            # true_preshape_palm_world_pose
+            grasp_group.create_dataset('true_preshape_palm_world_pose',
+                                       data=self.convert_pose_to_list(
+                                           req.true_preshape_palm_world_pose))
+
+            #6. Record all joint states
+            #desired_preshape_hithand_joint_state
+            grasp_group.create_dataset('desired_preshape_hithand_joint_state',
+                                       data=req.desired_preshape_hithand_joint_state.position)
+            #true_preshape_hithand_joint_state
+            grasp_group.create_dataset('true_preshape_hithand_joint_state',
+                                       data=req.true_preshape_hithand_joint_state.position)
+            #closed_hithand_joint_state
+            grasp_group.create_dataset('closed_hithand_joint_state',
+                                       data=req.closed_hithand_joint_state.position)
+            #lifted_hithand_joint_state
+            grasp_group.create_dataset('lifted_hithand_joint_state',
+                                       data=req.lifted_hithand_joint_state.position)
+
+            #8. Return response, context manager closes file
+            res = RecordGraspDataSimResponse()
+            res.success = True
+            return res
+
+    def handle_record_collision_data(self, req):
+        """ This records all the collision grasp data.
+        """
+        self.curr_object_name = req.object_name
+
+        # r+ : Read/write, file must exist
+        with h5py.File(self.grasp_data_file_name, 'r+') as grasp_file:
+            # get a descriptor for grasp trials
+            grasp_trials = grasp_file['recording_sessions'][self.curr_sess_name]['grasp_trials']
+
+            # Verify if object name already exists, if not create first
+            collision_gp = self.get_grasp_group(grasp_trials, grasp_class='collision')
+
+            # Update all metadata
+            self.update_all_metadata_collision(grasp_file,
+                                               num_grasps=len(req.preshapes_palm_mesh_frame_poses))
+
+            # Iterate through all grasps
+            for obj, palm, joints in zip(req.object_world_poses,
+                                         req.preshapes_palm_mesh_frame_poses,
+                                         req.preshape_hithand_joint_states):
+                # Convert poses to lists
+                obj_l = self.convert_pose_to_list(obj)
+                palm_l = self.convert_pose_to_list(palm)
+
+                # Increase collision id counter
+                self.collision_id += 1
+                collision_id_str = 'grasp_' + str(self.collision_id).zfill(4)
+
+                # Create new group under collision group for each grasp attempt
+                grasp_gp = collision_gp.create_group(collision_id_str)
+
+                # Object in world
+                grasp_gp.create_dataset('object_mesh_frame_world', data=obj_l)
+                # Palm pose w.r.t to object mesh frame
+                grasp_gp.create_dataset('palm_pose_mesh_frame', data=palm_l)
+                # Desired joint state
+                grasp_gp.create_dataset('joint_state', data=joints.position)
+
+    def create_record_grasp_trial_data_server(self):
+        rospy.Service('record_grasp_trial_data', RecordGraspDataSim,
+                      self.handle_record_grasp_trial_data)
+        rospy.loginfo('Service record_grasp_trial_data:')
+        rospy.loginfo('Ready to record your awesome grasp data.')
+
+    def create_record_collision_data_server(self):
+        rospy.Service('record_collision_data', RecordGraspDataSim,
+                      self.handle_record_collision_data)
+        rospy.loginfo('Service record_collision_data:')
+        rospy.loginfo('Ready to record your awesome collision data.')
+
+
+DEBUG = True
+
+if __name__ == '__main__':
+    rgd = RecordGraspData()
+    rgd.create_record_grasp_trial_data_server()
+    rgd.create_record_collision_data_server()
+    if DEBUG:
+        req = RecordCollisionDataRequest(object_name='kit_baking')
+        req.object_world_poses = [PoseStamped()]
+        req.preshape_hithand_joint_states = [JointState()]
+        req.preshapes_palm_mesh_frame_poses = [PoseStamped()]
+        rgd.handle_record_collision_data(req)
+
+        req = RecordGraspTrialDataRequest(object_name='kit_baking')
+        req.time_stamp = 'lol'
+        req.is_top_grasp = True
+        req.grasp_success_label = 1
+
+        # The true spawn pose in world frame
+        req.object_world_sim_pose = [PoseStamped()]
+        req.desired_preshape_palm_world_pose = [PoseStamped()]
+        req.true_preshape_palm_world_pose = [PoseStamped()]
+        # Hithand jointstates
+        req.desired_preshape_hithand_joint_state = [JointState()]
+        req.true_preshape_hithand_joint_state = [JointState()]
+        req.closed_hithand_joint_state = [JointState()]
+        req.lifted_hithand_joint_state = [JointState()]
+        rgd.handle_record_grasp_trial_data(req)
+
+    rospy.spin()
