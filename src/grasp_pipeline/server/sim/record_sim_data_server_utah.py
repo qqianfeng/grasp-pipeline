93714219d3ba8ed7ff28aa340ff616b3406db80f
v.mayer <vincent.mayer@agile-robots.com>
2021-02-24 19:22:15 +0100

    feat: simplified and added utah save server

---
+++ src/grasp_pipeline/server/sim/record_sim_data_server_utah.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import roslib
+roslib.load_manifest('grasp_pipeline')
+import rospy
+from grasp_pipeline.srv import *
+from geometry_msgs.msg import Pose, Quaternion
+from sensor_msgs.msg import JointState, CameraInfo
+import tf
+import numpy as np
+import h5py
+
+
+class RecordGraspData():
+    def __init__(self):
+        rospy.init_node('record_grasp_data_server')
+        self.num_grasps_per_object = rospy.get_param('~num_grasps_per_object', 10)
+        self.data_recording_path = rospy.get_param('~data_recording_path', '/home/vm/utah/')
+        self.grasp_file_name = self.data_recording_path + 'grasp_data_utah.h5'
+
+    def handle_record_grasp_data_utah(self, req):
+        with h5py.File(self.grasp_file_name, 'a') as hdf:
+            grasp_name = 'grasp_' + str(req.grasp_id).zfill(6)
+
+            hdf.create_dataset(grasp_name + '_config_obj', req.grasp_config_obj)
+            hdf.create_dataset(grasp_name + '_sparse_voxel', req.sparse_voxel)
+            hdf.create_dataset(grasp_name + '_dim_w_h_d', req.dim_w_h_d)
+            hdf.create_dataset(grasp_name + '_label', req.label)
+
+        res = SimGraspDataResponse(success=True)
+        return res
+
+    def create_record_data_server(self):
+        rospy.Service('record_grasp_data_utah', SimGraspData, self.handle_record_grasp_data_utah)
+        rospy.loginfo('Service record_grasp_data_utah:')
+        rospy.loginfo('Ready to record grasp data.')
+
+
+if __name__ == '__main__':
+    record_grasp_data = RecordGraspData()
+    record_grasp_data.create_record_data_server()
+    rospy.spin()
