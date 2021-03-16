9cbb6488bc9ef321b6db5375f0bedf61712a80f6
v.mayer <vincent.mayer@agile-robots.com>
2021-03-16 17:38:30 +0100

    feat: integrate metadata logging

--- src/grasp_pipeline/grasp_data_processing/vae_adapter_grasp_data.py
+++ src/grasp_pipeline/grasp_data_processing/vae_adapter_grasp_data.py
@@ -10,6 +10,7 @@ Storing all grasps object- and outcome-wise.
 """
 import h5py
 import os
+import pandas as pd
 
 P = 'positive'
 N = 'negative'
@@ -22,12 +23,68 @@ NC = 'no_collision'
 GPS = 'grasp_success_label'
 
 
+def log_to_csv(src_file, save_path):
+    data = {'names': [], 'positive': [], 'negative': [], 'collision': []}
+    # Add three rows positive negative collision
+    for obj in src_file.keys():
+        # Init counter
+        pos_cnt = 0
+        neg_cnt = 0
+        coll_cnt = 0
+
+        obj_gp = src_file[obj]
+        use_ix = False
+        if obj in data['names']:
+            ix = data['names'].index(obj)
+            use_ix = True
+        else:
+            data['names'].append(obj)
+        # Add new object as columns wit
+        for grasp_type in obj_gp.keys():
+            grasp_type_gp = obj_gp[grasp_type]
+            for grasp in grasp_type_gp.keys():
+                if grasp_type == 'positive':
+                    pos_cnt += 1
+                if grasp_type == 'negative':
+                    neg_cnt += 1
+                if grasp_type == 'collision':
+                    coll_cnt += 1
+
+            if use_ix:
+                if grasp_type == 'positive':
+                    data['positive'][ix] += pos_cnt
+                if grasp_type == 'negative':
+                    data['negative'][ix] += neg_cnt
+                if grasp_type == 'collision':
+                    data['collision'][ix] += coll_cnt
+            else:
+                if grasp_type == 'positive':
+                    data['positive'].append(pos_cnt)
+                if grasp_type == 'negative':
+                    data['negative'].append(neg_cnt)
+                if grasp_type == 'collision':
+                    data['collision'].append(coll_cnt)
+
+    # Save to disk
+    obj_names = data['names']
+    data.pop('names')
+    df = pd.DataFrame(data)
+    df.index = obj_names
+    df.to_csv(save_path)
+
+
 def log_grasp(src_grasp_gp, dest_grasp_gp, is_coll=False):
     if not is_coll:
         true_joint_conf = src_grasp_gp["true_preshape_joint_state"][()]
         des_joint_conf = src_grasp_gp["desired_preshape_joint_state"][()]
-        true_palm_mesh_frame = src_grasp_gp["true_preshape_palm_mesh_frame"][()]
-        des_palm_mesh_frame = src_grasp_gp["desired_preshape_palm_mesh_frame"][()]
+        if "true_preshape_palm_mesh_frame" in src_grasp_gp.keys():
+            true_palm_mesh_frame = src_grasp_gp["true_preshape_palm_mesh_frame"][()]
+            des_palm_mesh_frame = src_grasp_gp["desired_preshape_palm_mesh_frame"][()]
+        elif "true_preshape_palm_world_pose" in src_grasp_gp.keys():
+            true_palm_mesh_frame = src_grasp_gp["true_preshape_palm_world_pose"][()]
+            des_palm_mesh_frame = src_grasp_gp["desired_preshape_palm_world_pose"][()]
+        else:
+            raise Exception("Something is wrong, not world_frame no mesh_frame")
 
         dest_grasp_gp.create_dataset("true_preshape_joint_state", data=true_joint_conf)
         dest_grasp_gp.create_dataset("desired_preshape_joint_state", data=des_joint_conf)
@@ -35,7 +92,12 @@ def log_grasp(src_grasp_gp, dest_grasp_gp, is_coll=False):
         dest_grasp_gp.create_dataset("desired_preshape_palm_mesh_frame", data=des_palm_mesh_frame)
     else:
         des_joint_conf = src_grasp_gp["desired_joint_state"][()]
-        des_palm_mesh_frame = src_grasp_gp["desired_palm_pose_mesh_frame"][()]
+        if "desired_palm_pose_mesh_frame" in src_grasp_gp.keys():
+            des_palm_mesh_frame = src_grasp_gp["desired_palm_pose_mesh_frame"][()]
+        elif "desired_preshape_palm_world_pose" in src_grasp_gp.keys():
+            des_palm_mesh_frame = src_grasp_gp["desired_preshape_palm_world_pose"][()]
+        else:
+            raise Exception("Something is wrong, not world_frame no mesh_frame.")
 
         dest_grasp_gp.create_dataset("desired_preshape_joint_state", data=des_joint_conf)
         dest_grasp_gp.create_dataset("desired_preshape_palm_mesh_frame", data=des_palm_mesh_frame)
@@ -63,17 +125,18 @@ def log_idxs(path, obj, pos, neg, coll):
 
 if __name__ == "__main__":
 
-    base_path = '/home/vm/Documents/grasp_data'
+    base_path = '/home/vm/data/exp_data'
     #dst_path = os.path.join(os.path.split(base_path)[0], 'vae-grasp', 'grasp_data_vae.h5')
-    dst_path = '/home/vm/Documents/grasp_data/2021-03-08/grasp_data_vae.h5'
+    dst_path = '/home/vm/data/vae-grasp.h5'
     hdf_dst = h5py.File(dst_path, 'a')
 
     # go through all the dirs, each dir contains one grasp_data.h5
-    for dir in [os.listdir(base_path)[0]]:
+    for dir in sorted(os.listdir(base_path)):
         src_path = os.path.join(base_path, dir, 'grasp_data.h5')
         hdf_src = h5py.File(src_path, 'r')
 
         src_objs_gp = hdf_src[RC][RC1][GT]
+        print 'All objects: ', src_objs_gp.keys()
         for obj in src_objs_gp.keys():
             print('Processing object:', obj)
             # Grasp idxs
@@ -118,8 +181,12 @@ if __name__ == "__main__":
                 log_grasp(src_grasp_gp, dst_grasp_gp, is_coll=True)
 
             # Finally log the pos, neg coll idx to a txt file
-            #path = os.path.join(os.path.split(base_path)[0], 'vae-grasp', 'obj_metadata.txt')
-            #log_idxs(path, obj, pos_idx, neg_idx, coll_idx)
+            path = os.path.join(os.path.split(base_path)[0], 'vae-grasp', 'obj_metadata.txt')
+            log_idxs(path, obj, pos_idx, neg_idx, coll_idx)
+
+    # Create pandas dataframe and log
+    save_path = os.path.join(os.path.split(dst_path)[0], 'metadata.csv')
+    log_to_csv(hdf_dst, save_path)
 
     # close files
     hdf_src.close()
