f3697a3169ec7221ad55ac9b03b936791b322f84
v.mayer <vincent.mayer@agile-robots.com>
2021-03-19 10:58:54 +0100

    feat: add eval_vae to evaluate poses sampled from the vae

---
+++ src/grasp_pipeline/eval_vae.py
@@ -0,0 +1,17 @@
+""" This file is used to evaluate the model and sample grasps. 
+"""
+import torch
+import bps_torch
+pass
+
+# Spawn model
+
+# Get point cloud (mean-free, orientation of camera frame)
+
+# Compute BPS of point cloud
+
+# Build the model
+
+# Sample N latent variables and get the poses
+
+# Execute the grasps and record results
