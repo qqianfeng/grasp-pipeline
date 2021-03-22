e8f9e6683fe940be4cca34b47acb16e85e2cadb8
v.mayer <vincent.mayer@agile-robots.com>
2021-03-22 09:15:56 +0100

    fix: py3 encode pcd server

--- scripts/encode_pcd_with_bps.py
+++ scripts/encode_pcd_with_bps.py
@@ -20,9 +20,9 @@ class BPSEncoder():
         self.pcd_path = '/home/vm/object.pcd'
         self.enc_path = '/home/vm/pcd_enc.npy'
 
-        self.VISUALIZE = True
+        self.VISUALIZE = False
 
-    def handle_encode_pcd_with_bps(self, req):
+    def handle_encode_pcd_with_bps(self, req, res):
         obj_pcd = o3d.io.read_point_cloud(self.pcd_path)
         obj_np = np.asarray(obj_pcd.points)
 
@@ -38,6 +38,8 @@ class BPSEncoder():
             bps_pcd.points = o3d.utility.Vector3dVector(self.bps_np)
             bps_pcd.colors = o3d.utility.Vector3dVector(0.3 * np.ones(self.bps_np.shape))
             o3d.visualization.draw_geometries([obj_pcd, bps_pcd])
+
+        return True
 
 
 if __name__ == '__main__':
