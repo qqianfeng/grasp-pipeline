da0782e32ae8eb8acd0c170cb5b21af431e36d35
v.mayer <vincent.mayer@agile-robots.com>
2021-03-26 16:13:42 +0100

    feat; visualize grasps real data

--- src/grasp_pipeline/main/real/eval_vae_real.py
+++ src/grasp_pipeline/main/real/eval_vae_real.py
@@ -2,17 +2,17 @@
 """
 import torch
 
-from grasp_pipeline.grasp_client.grasp_sim_client import GraspClient
+from grasp_pipeline.grasp_client.grasp_real_client import GraspClient
 from grasp_pipeline.utils.metadata_handler import MetadataHandler
 
-n_poses = 10
+n_poses = 300
 
 grasp_client = GraspClient(is_rec_sess=False)
 metadata_handler = MetadataHandler()
 
 while True:
     # Reset
-    grasp_client.reset_hithand_and_panda()
+    #grasp_client.reset_hithand_and_panda()
 
     # Get point cloud (mean-free, orientation of camera frame)
     grasp_client.save_visual_data_and_segment_object(down_sample_pcd=False)
@@ -24,12 +24,13 @@ while True:
     palm_poses_obj_frame, joint_confs = grasp_client.infer_grasp_poses(n_poses=n_poses,
                                                                        visualize_poses=True)
 
+    # Show object and poses in o3d viewer
+    grasp_client.show_grasps_o3d_viewer(palm_poses_obj_frame)
+
     # Execute the grasps and record results
     for i in range(n_poses):
         if i != 0:
             grasp_client.reset_hithand_and_panda()
-
-            grasp_client.spawn_object(pose_type='same')
 
         grasp_arm_plan = grasp_client.grasp_from_inferred_pose(palm_poses_obj_frame[i],
                                                                joint_confs[i])
