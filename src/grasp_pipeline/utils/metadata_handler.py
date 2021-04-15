7ae16659f97c1b36bd5310afc7221112297fa662
v.mayer <vincent.mayer@agile-robots.com>
2021-04-15 11:34:41 +0200

    feat: added datahandler for final data gen

--- src/grasp_pipeline/utils/metadata_handler.py
+++ src/grasp_pipeline/utils/metadata_handler.py
@@ -92,6 +92,81 @@ class MetadataHandler():
         return object_metadata
 
 
+class MetadataHandlerFinalDataGen():
+    """ Simple class to help iterate through objects and 
+    """
+    def __init__(self, gazebo_objects_path='/home/vm/gazebo-objects/objects_gazebo'):
+        self.dset_obj_names = [OBJECTS_TO_GENERATE_DATA_FOR_AFTER_15_04_Laptop]
+
+        self.object_ix = -1
+        self.gazebo_objects_path = gazebo_objects_path
+
+    def get_total_num_objects(self):
+        return len(OBJECTS_TO_GENERATE_DATA_FOR_AFTER_15_04_Laptop)
+
+    def choose_next_grasp_object(self):
+        """ Iterates through all objects in all datasets and returns object_metadata. Gives a new object each time it is called.
+        """
+        choose_success = False
+        while (not choose_success):
+            try:
+                # When this is called a new object is requested
+                self.object_ix += 1
+
+                # Set some relevant variables
+                dset_obj_name = self.dset_obj_names[i]
+                split_name = dset_obj_name.split('_')
+                object_name = '_'.join(split_name[1:])
+                dataset_name = split_name[0]
+                assert dataset_name in ['kit', 'bigbird']
+                object_metadata = self.get_object_metadata(dataset_name, object_name)
+
+                rospy.loginfo('Trying to grasp object: %s' % object_metadata["name"])
+                choose_success = True
+
+                if dataset_name == 'kit' and object_metadata["name"] in KIT_OBJECTS_DATA_GENERATED:
+                    choose_success = False
+            except:
+                self.object_ix += 1
+
+        return object_metadata
+
+    def get_object_metadata(self, dataset_name, object_name):
+        object_path = os.path.join(self.gazebo_objects_path, dataset_name, object_name)
+        files = os.listdir(object_path)
+        collision_mesh = [s for s in files if "collision" in s][0]
+
+        # Create the final metadata dict to return
+        object_metadata = dict()
+        object_metadata["name"] = object_name
+        object_metadata["model_file"] = os.path.join(object_path, object_name + '.sdf')
+        object_metadata["collision_mesh_path"] = os.path.join(object_path, collision_mesh)
+        object_metadata["dataset"] = dataset_name
+        object_metadata["name_rec_path"] = dataset_name + '_' + object_name
+        object_metadata["mesh_frame_pose"] = None
+        object_metadata["seg_pose"] = None
+        object_metadata["aligned_pose"] = None
+        object_metadata["seg_dim_whd"] = None
+        object_metadata["aligned_dim_whd"] = None
+        object_metadata["spawn_angle_roll"] = 0
+
+        # Set the spawn height differently for the different datasets
+        if dataset_name == 'kit' or dataset_name == 'ycb':
+            object_metadata["spawn_height_z"] = 0.05
+        elif dataset_name == 'bigbird':
+            object_metadata["spawn_height_z"] = 0.02
+        else:
+            raise Exception("Bad dataset name.")
+
+        if dataset_name == 'kit' or object_name in YCB_PI_HALF_ROLL:
+            object_metadata["spawn_angle_roll"] = 1.57079632679
+
+        if object_name in SPAWN_HIGH_Z:
+            object_metadata["spawn_height_z"] += 0.1
+
+        return object_metadata
+
+
 if __name__ == '__main__':
     import open3d as o3d
     import numpy as np
