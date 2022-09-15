95fd71a31ec0294da2ad923f499f6029e1385de7
Yannick Burkhardt <yannick.burkhardt@agile-robots.com>
2022-09-15 13:47:05 +0200

    feat: add time analysis

--- scripts/grasp_pipeline_real.py
+++ scripts/grasp_pipeline_real.py
@@ -6,17 +6,30 @@ import time
 sys.path.append(os.path.join(os.environ['GRASP_PIPELINE_PATH'], 'src'))
 from graspInference import *
 
-n_poses = 100
+n_poses = 1000
 gi = GraspInference()
 
-for i in range(1):
+start = time.process_time()
+time_abs = 0.0
+
+i = 0
+while i < 100:
     palm_poses_obj_frame, joint_confs = gi.handle_infer_grasp_poses(n_poses)
 
-    for p in palm_poses_obj_frame:
-        print("palm_poses_obj_frame", p)
+    if 0:
+        for p in palm_poses_obj_frame:
+            print("palm_poses_obj_frame", p)
 
-    for j in joint_confs:
-        print("joint_confs", j)
+        for j in joint_confs:
+            print("joint_confs", j)
 
     palm_poses, joint_confs = gi.handle_evaluate_and_filter_grasp_poses(
         palm_poses_obj_frame, joint_confs, thresh=0.9)
+
+    i += 1
+
+    time_loop = time.process_time() - start - time_abs
+    print("Time of last cycle: ", time_loop)
+    time_abs += time_loop
+print("Overall time: ", time_abs)
+print("Avg time per cicle: ", time_abs/i, " | Cicles per second: ", i/time_abs, )
