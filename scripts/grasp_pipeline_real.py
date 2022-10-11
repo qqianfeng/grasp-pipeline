512d28ecef54c210372337ea1e4301cd6dcbf201
Yannick Burkhardt <yannick.burkhardt@agile-robots.com>
2022-10-11 17:39:49 +0200

    feat: transformation and control for whole pose incl. rotation

--- scripts/grasp_pipeline_real.py
+++ scripts/grasp_pipeline_real.py
@@ -15,10 +15,10 @@ from graspInference import *
 from ar_toolkit.robots import Diana7Robot
 
 # Transformations
-ee_R_hand = np.zeros((3, 3))
-ee_R_hand[0,2] = -1
-ee_R_hand[1,0] = -1
-ee_R_hand[2,1] = 1
+hand_R_ee = np.zeros((3, 3))
+hand_R_ee[2,0] = -1
+hand_R_ee[0,1] = 1
+hand_R_ee[1,2] = -1
 
 cam_R_ee = np.zeros((3, 3))
 cam_R_ee[0,1] = -1
@@ -26,9 +26,14 @@ cam_R_ee[1,0] = -1
 cam_R_ee[2,2] = -1
 cam_t_ee = np.array([0.1, 0, 0])
 
-base_R_ee= np.identity(3)
+base_R_ee = np.identity(3)
 base_R_ee[1,1] = -1
 base_R_ee[2,2] = -1
+
+hand_R_cam = np.zeros((3, 3))
+hand_R_cam[2,0] = 1
+hand_R_cam[1,1] = 1
+hand_R_cam[0,2] = -1
 
 # Move robot to home position
 robot = Diana7Robot("diana")
@@ -49,7 +54,12 @@ norm_rot_diff_list = []
 x = np.zeros(3)
 r = np.zeros(3)
 
-x_init = robot.get_cartesian_pose()
+x_init = robot.get_cartesian_pose()[:3, 3:4].flatten()
+
+# grasp_dict = gi.handle_infer_grasp_poses(n_poses)
+# p_success = gi.handle_evaluate_grasp_poses(grasp_dict)
+# p_best = np.argmax(p_success)
+# print(R.from_matrix(np.average(grasp_dict['rot_matrix'].detach().cpu().numpy(), axis=0)).as_euler('xyz'))
 
 i = 0
 while i < 100:
@@ -63,18 +73,22 @@ while i < 100:
         grasp_dict = gi.handle_infer_grasp_poses(n_poses)
         p_success = gi.handle_evaluate_grasp_poses(grasp_dict)
 
-    # Calculate difference to last palm pose
-    #if i > 0:
-    #    print("Relative offset of best pose: ", palm_poses_list[i-1]-palm_poses[0].pose)
+    # Rotate grasp poses into end effector frame
+    grasp_dict_rot = grasp_dict['rot_matrix'].detach().cpu().numpy() @ hand_R_ee.T
 
-    # Find pose closest to previous pose
-    #rot = R.from_matrix(grasp_dict['rot_matrix'].detach().cpu().numpy())
+    # Save rotation and translation of grasp poses
+    rot = R.from_matrix(grasp_dict_rot)
     pos = grasp_dict['transl'].detach().cpu().numpy() @ cam_R_ee.T
 
     # Update rotation and translation
-    cart_pose = robot.get_cartesian_pose() - x_init
-    x = cart_pose[:3, 3:4].flatten()
-    cart_pose_R = ee_R_hand @ cart_pose[:3, :3]
+    try:
+        cart_pose = robot.get_cartesian_pose()
+    except Exception as e:
+        print(e)
+        break
+
+    x = cart_pose[:3, 3:4].flatten() - x_init
+    cart_pose_R = cart_pose[:3, :3]
     r = R.from_matrix(cart_pose_R).as_rotvec()
 
     # Rotate grasps into camera frame
@@ -83,18 +97,18 @@ while i < 100:
 
     # Update pose (simulation)
     pos = pos - x
-    print(pos)
-    #rot = R.from_rotvec(rot.as_rotvec() - r)
+    rot = R.from_rotvec(rot.as_rotvec() - r)
     
     diff_pos = np.linalg.norm(pos, 1, axis=1)
-    #diff_rot = np.linalg.norm(rot.as_rotvec(), 1, axis=1)
+    diff_rot = np.linalg.norm(rot.as_rotvec(), 1, axis=1)
     
-    p_best = np.argmax(p_success - 5*diff_pos)# - 0.5*diff_rot)
+    p_best = np.argmax(p_success - 5*diff_pos - 5*diff_rot)
     norm_pos_diff_list.append(diff_pos[p_best])
-    #norm_rot_diff_list.append(diff_rot[p_best])
+    norm_rot_diff_list.append(diff_rot[p_best])
 
     palm_pos_list.append(pos[p_best])
-    #palm_rot_list.append(rot.as_rotvec()[p_best])
+    palm_rot_list.append(rot.as_rotvec()[p_best])
+
     p_best_list.append(p_success[p_best].copy())
 
     # Get max p
@@ -111,9 +125,9 @@ while i < 100:
     # Move robot towards grasp
     dx_update = pos[p_best]
 
-    dr_axis_update = np.array([1,0,0])#rot[p_best].as_rotvec()
-    #dr_axis_update /= np.linalg.norm(dr_axis_update)
-    dr_angle_update = 0#- np.linalg.norm(rot[p_best].as_rotvec())
+    dr_axis_update = rot[p_best].as_rotvec()
+    dr_axis_update /= np.linalg.norm(dr_axis_update)
+    dr_angle_update = np.linalg.norm(rot[p_best].as_rotvec())
 
     p = 0.1
     try:
@@ -126,10 +140,10 @@ print("Overall time: ", time_abs)
 print("Avg time per cicle: ", time_abs/i, " | Cicles per second: ", i/time_abs)
 
 plt.plot(palm_pos_list, label=["x", "y", "z"])
-#plt.plot(palm_rot_list, label=["rx", "ry", "rz"])
+plt.plot(palm_rot_list, label=["rx", "ry", "rz"])
 plt.plot(p_max_list, label="p_max")
 plt.plot(p_best_list, label="p_best")
 plt.plot(norm_pos_diff_list, label="delta_x")
-#plt.plot(norm_rot_diff_list, label="delta_rot")
+plt.plot(norm_rot_diff_list, label="delta_rot")
 plt.legend(loc="upper left")
 plt.show()
