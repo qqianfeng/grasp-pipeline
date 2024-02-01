4c5db58dc094830be346e664689c4cef66718a00
Qian Feng <qian.feng@agile-robots.com>
2024-02-01 18:42:08 +0100

    feat: add top 20 grasp w/o eval test

--- src/grasp_pipeline/main/sim/eval_ffhflow_sim.py
+++ src/grasp_pipeline/main/sim/eval_ffhflow_sim.py
@@ -40,8 +40,61 @@ with open('grasp_result.csv', 'wb') as file:
         rospy.loginfo("Now start experiement of object: %s" % obj_name)
         total_trials = 0
         success_trials = 0
-        for trial in range(NUM_TRIALS_PER_OBJ):
+        choose_top_rated_grasp = False # otherwise choose top 20 grasps
+        if choose_top_rated_grasp:
+            for trial in range(NUM_TRIALS_PER_OBJ):
+                # Reset
+                grasp_client.reset_hithand_and_panda()
+
+                # Spawn model
+                # grasp_client.spawn_object(pose_type='init', pose_arr=[0.75, 0, 0, 0, 0, -2.57])
+                grasp_client.spawn_object(pose_type="random")
+
+                # Get point cloud (mean-free, orientation of camera frame)
+                grasp_client.save_visual_data_and_segment_object(down_sample_pcd=False)
+
+                # Compute BPS of point cloud, stores encoding to disk
+                grasp_client.encode_pcd_with_bps()
+
+                # Sample N latent variables and get the poses
+                palm_poses_obj_frame, joint_confs = grasp_client.infer_flow_grasp_poses(visualize_poses=True)
+
+                # Evaluate the generated poses according to the FFHEvaluator
+                palm_poses_obj_frame, joint_confs = grasp_client.evaluate_and_remove_grasps(
+                    palm_poses_obj_frame, joint_confs, thresh=FILTER_THRESH, visualize_poses=True)
+
+                palm_poses_obj_frame = palm_poses_obj_frame[:FILTER_NUM_GRASPS]
+                joint_confs = joint_confs[:FILTER_NUM_GRASPS]
+
+                # Execute the grasps and record results
+                rospy.loginfo("The {} time of trial out of {}".format(trial, NUM_TRIALS_PER_OBJ))
+                for i in range(FILTER_NUM_GRASPS):
+                    rospy.loginfo('start a new grasp')
+                    if i != 0:
+                        grasp_client.reset_hithand_and_panda()
+
+                        grasp_client.spawn_object(pose_type='same')
+                    # idx = np.random.randint(0, len(joint_confs))
+                    idx = i
+                    try:
+                        grasp_executed, grasp_label = grasp_client.grasp_from_inferred_pose(palm_poses_obj_frame[idx],
+                                                                        joint_confs[idx])
+                    except IndexError:
+                        print('palm_poses_obj_frame:', len(palm_poses_obj_frame))
+                        print('joint_confs:', len(joint_confs))
+                        print(idx)
+                    if grasp_executed:
+                        total_trials += 1
+                        if grasp_label == 1:
+                            success_trials += 1
+                        print('current status of %s success grasps out of %s trials', success_trials, total_trials)
+                        grasp_client.record_grasp_trial_data_client()
+                        break
+        else: # choose top 20 grasos
+            # for trial in range(NUM_TRIALS_PER_OBJ):
             # Reset
+            with_evaluator = False
+            FILTER_NUM_GRASPS = 50
             grasp_client.reset_hithand_and_panda()
 
             # Spawn model
@@ -57,24 +110,29 @@ with open('grasp_result.csv', 'wb') as file:
             # Sample N latent variables and get the poses
             palm_poses_obj_frame, joint_confs = grasp_client.infer_flow_grasp_poses(visualize_poses=True)
 
+            if with_evaluator:
             # Evaluate the generated poses according to the FFHEvaluator
-            palm_poses_obj_frame, joint_confs = grasp_client.evaluate_and_remove_grasps(
-                palm_poses_obj_frame, joint_confs, thresh=FILTER_THRESH, visualize_poses=True)
+                palm_poses_obj_frame, joint_confs = grasp_client.evaluate_and_remove_grasps(
+                    palm_poses_obj_frame, joint_confs, thresh=FILTER_THRESH, visualize_poses=True)
 
-            palm_poses_obj_frame = palm_poses_obj_frame[:FILTER_NUM_GRASPS]
-            joint_confs = joint_confs[:FILTER_NUM_GRASPS]
+                palm_poses_obj_frame = palm_poses_obj_frame[:FILTER_NUM_GRASPS]
+                joint_confs = joint_confs[:FILTER_NUM_GRASPS]
+            else: # randomly choose 20 grasps
+
+                palm_poses_obj_frame = palm_poses_obj_frame[:FILTER_NUM_GRASPS]
+                joint_confs = joint_confs[:FILTER_NUM_GRASPS]
 
             # Execute the grasps and record results
-            rospy.loginfo("The {} time of trial out of {}".format(trial, NUM_TRIALS_PER_OBJ))
-            is_skipped = False
             for i in range(FILTER_NUM_GRASPS):
                 rospy.loginfo('start a new grasp')
                 if i != 0:
                     grasp_client.reset_hithand_and_panda()
 
                     grasp_client.spawn_object(pose_type='same')
-                # idx = np.random.randint(0, len(joint_confs))
-                idx = i
+                if with_evaluator:
+                    idx = i
+                else: # Here we evaluate with generative model alone, so index is randomly chosen.
+                    idx = np.random.randint(0, len(joint_confs))
                 try:
                     grasp_executed, grasp_label = grasp_client.grasp_from_inferred_pose(palm_poses_obj_frame[idx],
                                                                     joint_confs[idx])
@@ -82,13 +140,13 @@ with open('grasp_result.csv', 'wb') as file:
                     print('palm_poses_obj_frame:', len(palm_poses_obj_frame))
                     print('joint_confs:', len(joint_confs))
                     print(idx)
-                is_skipped = not grasp_executed
                 if grasp_executed:
                     total_trials += 1
                     if grasp_label == 1:
                         success_trials += 1
                     print('current status of %s success grasps out of %s trials', success_trials, total_trials)
                     grasp_client.record_grasp_trial_data_client()
+                if total_trials >= 20: # Every object runs 20 times trial.
                     break
 
         writer.writerow([obj_name,success_trials,total_trials])
